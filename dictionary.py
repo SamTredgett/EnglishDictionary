82b6d31bcf27f478596bd27ec9175a987b5ba641
Sam Tredgett <sam.tredgett@gmail.com>
2021-09-08 13:23:40 +0100

    Included better decision making and error handling to prevent code smells using try else statements. Also structured out the ability for users to input into the program for suggeted words rather than assume them.

--- dictionary.py
+++ dictionary.py
@@ -1,17 +1,25 @@
 import json
 import difflib
+from os import close
 def word_search(user_in: str):
     
     data = json.load(open('data.json', 'r'))
-    closest_word = difflib.get_close_matches(user_in,data.keys())[0]
-    if user_in in data:
+    if user_in in data.keys():
         print(f'Definition for {user_in}')
         for item in data[user_in]:
           print(f'-> {item}')
-    elif closest_word in data:
-        print(f'Definition for {closest_word}')
-        for item in data[closest_word]:
-          print(f'-> {item}')
+
+    elif len(difflib.get_close_matches(user_in,data.keys(),cutoff=0.8)) > 0:
+        closest_word = difflib.get_close_matches(user_in,data.keys(),cutoff=0.8)[0]
+        print(f"You searched for '{user_in}', but this doesn't return any results.")
+        user_choice = input(f'Did you mean to search for {closest_word}? Y/N: ')
+        # Take a user choice to see if they'd like to proceed with suggested word
+        if user_choice.lower() == 'y':
+            print(f'Definition for {closest_word}')
+            for item in data[closest_word]:
+                print(f'-> {item}')
+        else:
+            word_search(input('Please enter a new word: '))
     else:
         print('No dictionary entry found')
         redo = input('Try again? type Y or N: ')
